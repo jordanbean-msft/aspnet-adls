94c1a508db71b32db6cb07d159341436b8134431
Jordan Bean <jordanbean@microsoft.com>
2022-06-16 12:51:03 -0500

    thyme

--- src/java/src/main/java/com/microsoft/azure/aspnetadls/ServletInitializer.java
+++ src/java/src/main/java/com/microsoft/azure/aspnetadls/ServletInitializer.java
@@ -1,17 +1,69 @@
 package com.microsoft.azure.aspnetadls;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Description;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @SpringBootApplication
 @ComponentScan("com.microsoft.azure.aspnetadls")
 public class ServletInitializer extends SpringBootServletInitializer {
+  private ApplicationContext applicationContext;
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(AspnetAdlsApplication.class);
   }
 
+  public void setApplicationContext(final ApplicationContext applicationContext)
+      throws BeansException {
+    this.applicationContext = applicationContext;
+  }
+
+  @Bean
+  public SpringResourceTemplateResolver templateResolver() {
+    // SpringResourceTemplateResolver automatically integrates with Spring's own
+    // resource resolution infrastructure, which is highly recommended.
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(this.applicationContext);
+    templateResolver.setPrefix("/WEB-INF/templates/");
+    templateResolver.setSuffix(".html");
+    // HTML is the default value, added here for the sake of clarity.
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    // Template cache is true by default. Set to false if you want
+    // templates to be automatically updated when modified.
+    templateResolver.setCacheable(true);
+    return templateResolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    // SpringTemplateEngine automatically applies SpringStandardDialect and
+    // enables Spring's own MessageSource message resolution mechanisms.
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+    // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+    // speed up execution in most scenarios, but might be incompatible
+    // with specific cases when expressions in one template are reused
+    // across different data types, so this flag is "false" by default
+    // for safer backwards compatibility.
+    templateEngine.setEnableSpringELCompiler(true);
+    return templateEngine;
+  }
+
+  @Bean
+  public ThymeleafViewResolver viewResolver() {
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(templateEngine());
+    return viewResolver;
+  }
 }
